7509eca66b852e410aeab7f8638dfefd1d06e2c2
Hamed <mehrabi.hamed@outlook.com>
2023-02-18 12:48:18 +0000

    Stage 01 update

--- setup.py
+++ setup.py
@@ -4,23 +4,30 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "Project-DVC-FoodVision"
+AUTHOR_USER_NAME = "Hamed"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = []
+LIST_OF_REQUIREMENTS = ["black",
+                        "dvc==2.45.1",
+                        "tqdm",
+                        "tensorflow==2.8.0",
+                        "joblib",
+                        "Pillow",
+                        "scipy"
+                        ]
 
 
 setup(
     name=SRC_REPO,
-    version="0.0.1",
+    version="0.0.2",
     author=AUTHOR_USER_NAME,
     description="A small package for DVC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/hamehrabi/Project-DVC-FoodVision",
+    author_email="mehrabi.hamed@outlook.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=LIST_OF_REQUIREMENTS
 )
--- src/utils/common.py
+++ src/utils/common.py
@@ -1,24 +1,70 @@
 import os
 import yaml
 import logging
+import shutil
+from tqdm import tqdm
 import time
-import pandas as pd
-import json
 
-def read_yaml(path_to_yaml: str) -> dict:
-    with open(path_to_yaml) as yaml_file:
+def read_yaml_file(path_to_yaml_file: str) -> dict:
+    """it reads yaml file into dictionory
+
+    Args:
+        path_to_yaml_file (str): path of the yaml sikh with different agendars
+
+    Returns:
+        dict: returns the key-value pair from yaml file
+    """
+    with open(path_to_yaml_file) as yaml_file:
         content = yaml.safe_load(yaml_file)
-    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+
+    logging.info(f"content from {path_to_yaml_file} read successfully!!")
     return content
 
-def create_directories(path_to_directories: list) -> None:
-    for path in path_to_directories:
-        os.makedirs(path, exist_ok=True)
-        logging.info(f"created directory at: {path}")
+
+def create_directories(list_of_directories: list) -> None:
+    """Responsible for creating directories whenerver required
+
+    Args:
+        list_of_directories (list): [description]
+    """
+    for dir_path in list_of_directories:
+        os.makedirs(dir_path, exist_ok=True)
+        logging.info(f"Directory is created at {dir_path}")
 
 
-def save_json(path: str, data: dict) -> None:
-    with open(path, "w") as f:
-        json.dump(data, f, indent=4)
+def copy_files(source_data_dir: str, local_data_dir: str) -> None:
+    """Copies file from source to destination directory
 
-    logging.info(f"json file saved at: {path}")
+    Args:
+        source_data_dir (str): source data directory
+        local_data_dir (str): local data directory
+    """
+    list_of_files = os.listdir(source_data_dir)
+    N = len(list_of_files)
+
+    for filename in tqdm(
+        list_of_files,
+        total=N,
+        desc=f"copying file from {source_data_dir} to {local_data_dir}",
+        colour="green",
+    ):
+        src = os.path.join(source_data_dir, filename)
+        dest = os.path.join(local_data_dir, filename)
+        shutil.copy(src, dest)
+
+    logging.info(
+        f"all the files has been copied from {source_data_dir} to {local_data_dir}"
+    )
+
+def get_timestamp(name: str) -> str:
+    """create unique name with timestamp
+
+    Args:
+        name (str): name of file or directory
+
+    Returns:
+        str: unique name with timestamp
+    """
+    timestamp = time.asctime().replace(" ", "_").replace(":", ".")
+    unique_name = f"{name}_at_{timestamp}"
+    return unique_name
