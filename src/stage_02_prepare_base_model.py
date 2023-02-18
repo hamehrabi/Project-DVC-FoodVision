703ee74a0e539d9de1f01b533481fbe8bce0987d
Hamed <mehrabi.hamed@outlook.com>
2023-02-18 16:59:50 +0000

    stage 02 updated

---
+++ src/stage_02_prepare_base_model.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+from src.utils.common import read_yaml_file, create_directories
+from src.utils.model import get_VGG16_model, prepare_full_model
+from tqdm import tqdm
+import logging
+
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+def prepare_base_model(config_path: str, params_path: str) -> None:
+    """prepares and saves the untrained model 
+    that can be used for training later on the given data
+
+    Args:
+        config_path (str): path to configuration file
+        params_path (str): path to params file
+    """
+    config = read_yaml_file(config_path)
+    params = read_yaml_file(params_path)
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+
+    base_model_dir = artifacts["BASE_MODEL_DIR"]
+    base_model_name = artifacts["BASE_MODEL_NAME"]
+
+    base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
+    create_directories([base_model_dir_path])
+
+    base_model_path = os.path.join(base_model_dir_path, base_model_name)
+
+    base_model = get_VGG16_model(
+        input_shape=params["IMAGE_SIZE"], 
+        model_path=base_model_path)
+
+    full_model = prepare_full_model(
+        base_model,
+        learning_rate=params["LEARNING_RATE"],
+        CLASSES = 101,
+        freeze_all=True,
+        freeze_till=None)
+
+    updated_full_model_path = os.path.join(base_model_dir_path, artifacts["UPDATED_BASE_MODEL_NAME"])
+
+    full_model.save(updated_full_model_path)
+    logging.info(f"full untrained model is saved at {updated_full_model_path}")
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(">>>>> stage two started <<<<<")
+        prepare_base_model(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(">>>>> stage two completed! base model is created and saved successfully <<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
---
+++ src/utils/model.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+import logging
+import io
+from tensorflow.python.keras.backend import flatten
+from src.utils.common import get_timestamp
+import os
+
+
+def _get_model_summary(model):
+    with io.StringIO() as stream:
+        model.summary(
+            print_fn=lambda x: stream.write(f"{x}\n")
+        )
+        summary_str = stream.getvalue()
+    return summary_str
+
+
+def get_VGG16_model(input_shape: list, model_path: str) -> tf.keras.models.Model:
+    """saving and returning the base model extracted from VGG16 model
+
+    Args:
+        input_shape (list): shape of the input image
+        model_path (str): path to save the base model   
+
+    Returns:
+        tf.keras.models.Model: base model
+    """
+    model = tf.keras.applications.vgg16.VGG16(
+        input_shape=input_shape,
+        weights='imagenet',
+        include_top=False
+    )
+    
+    logging.info(f"VGG16 base model summary:\n{_get_model_summary(model)}")
+    model.save(model_path)
+    logging.info(f"VGG16 base model saved at: {model_path}")
+    return model
+
+
+def prepare_full_model(base_model, learning_rate,
+        CLASSES = 101, freeze_all=True, freeze_till=None) -> tf.keras.models.Model:
+    """prepares the complete transfer learning model aarchitecture
+
+    Args:
+        base_model (tf.keras.models.model): base VGG16 model
+        learning_rate (float): learning rate for training model
+        CLASSES (int, optional): no of classes to train for. Defaults to 2.
+        freeze_all (bool, optional): freezes all the layers to make them untrainable. Defaults to True.
+        freeze_till (int, optional): this is the values which the extent of layers to which we want to train. Defaults to None.
+
+    Returns:
+        tf.keras.models.Model: full model architecture ready to be trained
+    """
+    if freeze_all:
+        for layer in base_model.layers:
+            layer.trainable = False
+    elif (freeze_till is not None) and (freeze_till > 0):
+        for layer in base_model.layers[:-freeze_till]:
+            layer.trainable = False
+
+    ## add our layers to the base model
+    flatten_in = tf.keras.layers.Flatten()(base_model.output)
+
+    prediction = tf.keras.layers.Dense(
+        units=CLASSES,
+        activation="softmax"
+    )(flatten_in)
+
+    full_model = tf.keras.models.Model(
+        inputs = base_model.input,
+        outputs = prediction
+    )
+
+    full_model.compile(
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        loss = tf.keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy"]
+    )
+
+    logging.info("custom model is compiled and ready to be trained")
+
+    logging.info(f"full model summary {_get_model_summary(full_model)}")
+    return full_model
+
+
+def load_full_model(untrained_full_model_path: str) -> tf.keras.models.Model:
+    model = tf.keras.models.load_model(untrained_full_model_path)
+    logging.info(f"untrained models is read from: {untrained_full_model_path}")
+    logging.info(f"untrained full model summary: \n{_get_model_summary(model)}")
+    return model
+
+def get_unqique_path_to_save_model(trained_model_dir: str, 
+    model_name: str = "model") -> str:
+    timestamp = get_timestamp(name=model_name)
+    unique_model_name = f"{timestamp}_.h5"
+    unique_model_path = os.path.join(trained_model_dir, unique_model_name)
+
+    return unique_model_path
+
